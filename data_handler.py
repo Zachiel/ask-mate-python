21b743d67451c01defeb005d2c344b9477570731
Zachiel <ezehiel182@gmail.com>
2022-12-29 23:55:47 +0100

    started data handling file, improved homepage

---
+++ data_handler.py
@@ -0,0 +1,35 @@
+"""Data read/write and manipulation functions."""
+import csv
+import datetime
+from typing import Any
+
+HEADERS_QUESTION: list[str] = ['id', 'submission_time', 'view_number',
+                    'vote_number', 'title', 'message', 'image']
+HEADERS_ANSWER: list[str] = ['id', 'submission_time', 'vote_number',
+                            'question_id', 'message', 'image']
+
+def get_data_from_file(filename: str) -> list[Any]:
+    """Read data from file into list of dictionaries."""
+    with open(filename, 'r', encoding='UTF-8') as data:
+        data_list = []
+        reader = csv.DictReader(data)
+        for item in reader:
+            data_list.append(item)
+        return data_list
+
+
+def write_data_to_file(filename: str, data_dict: list[dict[str, str]]) -> None:
+    """Write dictionaries to file."""
+
+
+def sorter(data_dict: list[dict[str, str]], sort_by='submission_time',
+           descending=True) -> list[dict[str, str]]:
+    """Sort given data by specific header."""
+
+
+def convert_date(unix_date: str) -> str:
+    """Convert unix date into human format."""
+
+
+def generate_id() -> str:
+    """Generate new id."""
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 """AskMate server route management."""
 from flask import Flask, render_template
+import data_handler
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -7,7 +8,10 @@ app = Flask(__name__, static_url_path='/static')
 @app.route("/")
 def hello():
     """Main page route."""
-    return render_template('base.html')
+    headers_question = data_handler.HEADERS_QUESTION
+    headers_answer = data_handler.HEADERS_ANSWER
+    return render_template('index.html', headers_question=headers_question,
+                           headers_answer=headers_answer)
 
 
 if __name__ == "__main__":
