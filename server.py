fd5a08065b008b444037bf63c4265d6b0bb6f67d
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-01-12 16:11:14 +0100

    added slash by url cuz wont work else

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ def hello():
                            time_passed=data_handler.how_much_time_passed,
                            comment_count=comment_count)
 
-@app.route("/question/<question_id>")
+@app.route("/question/<question_id>/")
 def question(question_id):
     questions = data_handler.get_data_from_file(
         'sample_data/question.csv')
