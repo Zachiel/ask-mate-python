a786b242a1843a50e4107af8a4f060f0f15a2a94
arkadiuszwoznica <115310756+arkadiuszwoznica@users.noreply.github.com>
2023-01-13 22:42:38 +0100

    return to question, not list
    after deleting of answer is returning to question page, not main page

--- server.py
+++ server.py
@@ -59,7 +59,7 @@ def delete_question(question_id):
 @app.route("/question/<question_id>/<aid>/delete_answer", methods=["POST"])
 def delete_answer(question_id, aid):
     data_handler.delete_specific_answer(aid)
-    return redirect("/list")
+    return redirect("/question/" + question_id)
 
 
 @app.route("/question/<question_id>/new-answer", methods=['POST', 'GET'])
@@ -99,26 +99,6 @@ def add_new_question():
 
         data_handler.write_data_to_file(data_handler.HEADERS_QUESTION, data_handler.QUESTION_PATH, new_question)
         return redirect('/list')
-    
-@app.route("/question/<question_id>/edit", methods=['POST','GET'])
-def edit(question_id):
-    questions = data_handler.get_data_from_file(
-        'sample_data/question.csv')
-    question_to_send = ''
-    for question in questions:
-        if question['id'] == question_id:
-            question_to_send = question
-            break
-        
-    if request.method == 'POST':
-        question['title'] = request.form.get("title")
-        question['message'] = request.form.get("message")
-        data_handler.delete_question_from_file_by_id('sample_data/question.csv', question_id)
-        data_handler.write_data_to_file(HEADERS_QUESTION, data_handler.QUESTION_PATH, question)
-        return redirect('/question/'+question_id)
-    
-    return render_template('edit_question.html',
-                           question=question_to_send)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
