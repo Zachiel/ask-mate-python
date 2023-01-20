32e20c1c1e23056ab54ea634eccc74ae357c0348
Adam Nowicki <adamnow83@gmail.com>
2023-01-20 16:28:30 +0000

    still bugs

--- dupaprinting.py
+++ dupaprinting.py
@@ -2,12 +2,7 @@ import psycopg2
 import psycopg2.extras
 import data_handler
 import database_common
-
-# dict_cur.execute("SELECT * FROM answer")
-# rec = dict_cur.fetchall()
-# dict_cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
-
+import datetime
 
 conn = psycopg2.connect("dbname=cc_ask_mate user=cc_ask_mate password=test123")
 cur = conn.cursor()
@@ -23,25 +18,30 @@ question_sql = [dict(zip(column_names, row))
         for row in cur.fetchall()]
 conn.close()
 
-
-def zip_sql_to_dict(cursor):
-        pass
-
-
-# print(question_sql)
-# print(data_handler.get_data_from_file(
-#         'sample_data/answer.csv'))
+def time_now():
+    time_now  = datetime.now()
+    time_now = int(round(datetime.timestamp(time_now), 0))
+    return time_now
 
 
-@database_common.connection_handler
-def get_questions(cursor):
-    query = """
-        SELECT *
-        FROM question"""
-    cursor.execute(query)
-    desc = cursor.description
-    column_names = [col[0] for col in desc]
-    question_sql = [dict(zip(column_names, row)) for row in cursor.fetchall()]
-    return (question_sql)
 
-get_questions()
+
+# @database_common.connection_handler
+def count_comments() -> dict[str, int]:
+    """Get comment count for each question."""
+    comments_count = {}
+    questions = data_handler.get_questions()
+    answers = data_handler.get_answers()
+    for question in questions:
+        for key, value in question.items():
+            if key == 'question_id':
+                comments_count.update({value: 0})
+    for answer in answers:
+        for key, value in answer.items():
+            if key == 'question_id':
+                comments_count[value] += 1
+    return comments_count
+
+
+
+print(count_comments())
