ca991ec04a90cb826df46aebd2f3403d8699ba53
Zachiel <ezehiel182@gmail.com>
2023-01-25 12:46:32 +0100

    Added docstrings and formatted some code

--- database_common.py
+++ database_common.py
@@ -1,4 +1,5 @@
-# pylint: disable=line-too-long, consider-using-f-string, unused-import
+"""Database connection functions."""
+# pylint: disable=line-too-long, unused-import
 import os
 import sys
 import psycopg2
@@ -10,14 +11,10 @@ def get_connection_string():
     password = os.environ.get('PSQL_PASSWORD')
     host = os.environ.get('PSQL_HOST')
     database_name = os.environ.get('PSQL_DB_NAME')
-    env_variables_defined = user_name and password and host and database_name
+    env_variables_defined = (user_name and password and
+                                        host and database_name)
     if env_variables_defined:
-        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
-            user_name=user_name,
-            password=password,
-            host=host,
-            database_name=database_name
-        )
+        return f'postgresql://{user_name}:{password}@{host}/{database_name}'
     raise KeyError('Some necessary environment variable(s) are not defined')
 
 
