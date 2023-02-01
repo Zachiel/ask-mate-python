81a33bf12cfe2feae6a2e26bc410a580cbdb4ed9
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-02-01 01:15:13 +0100

    fixed functions

--- hashing.py
+++ hashing.py
@@ -4,6 +4,7 @@ def hash_password(password):
     bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
+    hash = hash.decode('utf-8')
     return hash
 
 @database_common.connection_handler
@@ -11,9 +12,7 @@ def check_login_password(cursor, username, password):
     query = """
     SELECT password FROM accounts
     WHERE username = %s"""
-    
     cursor.execute(query, (username, ))
     db_hashed_password = cursor.fetchone()
-    user_pw_in_bytes = password.encode('utf-8')
-    
-    return bcrypt.checkpw(user_pw_in_bytes, db_hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), 
+                          db_hashed_password['password'].encode('utf-8'))
