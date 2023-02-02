73157c2b8b485f5f03c0bc33357fd375fefeb093
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-02-02 19:26:00 +0100

    File cleaning

--- sample_data/cc_ask_mate.session.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS accounts (
-    id serial NOT NULL,
-    username varchar(50) NOT NULL,
-    password varchar(255) NOT NULL,
-    email varchar(100),
-    PRIMARY KEY (id)
-
-);
