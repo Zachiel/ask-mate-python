5d05e24c17beb158bb0a63147917bdd0cd6d5a4c
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-02-02 18:27:38 +0100

    creating table accounts sql

---
+++ sample_data/accounts_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS accounts (
+    id serial NOT NULL,
+    username varchar(50) NOT NULL,
+    password varchar(255) NOT NULL,
+    email varchar(100) NOT NULL,
+    fname varchar(100),
+    lname varchar(100),
+    PRIMARY KEY (id)
+
+);
