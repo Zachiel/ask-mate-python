c15512bcb71daeb5f8ee2cf27bf8b2e88026a076
arkadiuszwoznica <115310756+arkadiuszwoznica@users.noreply.github.com>
2023-02-02 13:04:58 +0100

    tag in question, remove question_tag table

--- sample_data/askmatepart2-sample-data.sql
+++ sample_data/askmatepart2-sample-data.sql
@@ -24,7 +24,8 @@ CREATE TABLE question (
     vote_number integer,
     title text,
     message text,
-    image text
+    image text,
+    tag_id integer
 );
 
 DROP TABLE IF EXISTS public.answer;
@@ -48,11 +49,6 @@ CREATE TABLE comment (
 );
 
 
-DROP TABLE IF EXISTS public.question_tag;
-CREATE TABLE question_tag (
-    question_id integer NOT NULL,
-    tag_id integer NOT NULL
-);
 
 DROP TABLE IF EXISTS public.tag;
 CREATE TABLE tag (
@@ -91,7 +87,7 @@ ALTER TABLE ONLY comment
 ALTER TABLE ONLY question_tag
     ADD CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES tag(id);
 
-INSERT INTO question VALUES (0, '2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL);
+INSERT INTO question VALUES (0, '2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL, 0);
 INSERT INTO question VALUES (1, '2017-04-29 09:19:00', 15, 9, 'Wordpress loading multiple jQuery Versions', 'I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $(".myBook").booklet();
 
 I could easy managing the loading order with wp_enqueue_script so first I load jquery then I load booklet so everything is fine.
@@ -100,9 +96,10 @@ BUT in my theme i also using jquery via webpack so the loading order is now foll
 
 jquery
 booklet
-app.js (bundled file with webpack, including jquery)', 'images/image1.png');
+app.js (bundled file with webpack, including jquery)', 'images/image1.png', 2);
 INSERT INTO question VALUES (2, '2017-05-01 10:41:00', 1364, 57, 'Drawing canvas with an image picked with Cordova Camera Plugin', 'I''m getting an image from device and drawing a canvas with filters using Pixi JS. It works all well using computer to get an image. But when I''m on IOS, it throws errors such as cross origin issue, or that I''m trying to use an unknown format.
-', NULL);
+', NULL, 1);
+
 SELECT pg_catalog.setval('question_id_seq', 2, true);
 
 INSERT INTO answer VALUES (1, '2017-04-28 16:49:00', 4, 1, 'You need to use brackets: my_list = []', NULL);
@@ -113,11 +110,9 @@ INSERT INTO comment VALUES (1, 0, NULL, 'Please clarify the question as it is to
 INSERT INTO comment VALUES (2, NULL, 1, 'I think you could use my_list = list() as well.', '2017-05-02 16:55:00');
 SELECT pg_catalog.setval('comment_id_seq', 2, true);
 
-INSERT INTO tag VALUES (1, 'python');
-INSERT INTO tag VALUES (2, 'sql');
-INSERT INTO tag VALUES (3, 'css');
-SELECT pg_catalog.setval('tag_id_seq', 3, true);
+INSERT INTO tag VALUES (1, 'None');
+INSERT INTO tag VALUES (2, 'python');
+INSERT INTO tag VALUES (3, 'sql');
+INSERT INTO tag VALUES (4, 'css');
+SELECT pg_catalog.setval('tag_id_seq', 4, true);
 
-INSERT INTO question_tag VALUES (0, 1);
-INSERT INTO question_tag VALUES (1, 3);
-INSERT INTO question_tag VALUES (2, 3);
